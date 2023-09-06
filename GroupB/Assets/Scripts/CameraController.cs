cd2110581fcc8dcff97c277eb3cdc29b97f40716
shiffon7343 <game-mu-ka-mi-family@docomo.ne.jp>
2023-09-06 16:22:50 +0900

    カメラの移動

--- GroupB/Assets/Scripts/CameraController.cs
+++ GroupB/Assets/Scripts/CameraController.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
-    private GameObject mainCamera;              //メインカメラ格納用
-    private GameObject playerObject;            //回転の中心となるプレイヤー格納用
-    public float rotateSpeed = 2.0f;            //回転の速さ
 
+    private GameObject mainCamera;              //メインカメラ格納用
+    public float rotateSpeed = 2.0f;
+
+
+    float x;
+    float z;
+    float moveSpeed = 2.0f;
+    Rigidbody rb;
     //呼び出し時に実行される関数
     void Start()
     {
-        //メインカメラとユニティちゃんをそれぞれ取得
+        //メインカメラとプレイヤーをそれぞれ取得
         mainCamera = Camera.main.gameObject;
-        playerObject = GameObject.Find("Player");
+       
     }
 
 
@@ -25,7 +32,7 @@ public class CameraController : MonoBehaviour
             //rotateCameraの呼び出し
             rotateCamera();
         }
-        
+        MoveCamera();
     }
 
     //カメラを回転させる関数
@@ -35,7 +42,32 @@ public class CameraController : MonoBehaviour
         Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * rotateSpeed, Input.GetAxis("Mouse Y") * rotateSpeed, 0);
 
         //transform.RotateAround()をしようしてメインカメラを回転させる
-        mainCamera.transform.RotateAround(playerObject.transform.position, Vector3.up, angle.x);
-        mainCamera.transform.RotateAround(playerObject.transform.position, transform.right, -angle.y);
+        mainCamera.transform.RotateAround(mainCamera.transform.position, Vector3.up, angle.x);
+        mainCamera.transform.RotateAround(mainCamera.transform.position, Vector3.right, -angle.y);
+        mainCamera.transform.localEulerAngles = new Vector3(mainCamera.transform.localEulerAngles.x, mainCamera.transform.localEulerAngles.y, 0.0f);
+    }
+
+    void MoveCamera()
+    {
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
+        {
+            var direction = Quaternion.Euler(mainCamera.transform.eulerAngles) * Vector3.forward;
+            transform.position += new Vector3(direction.x, 0, direction.z) * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            var direction = Quaternion.Euler(mainCamera.transform.eulerAngles) * Vector3.back;
+            transform.localPosition += new Vector3(direction.x, 0, direction.z) * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            var direction = Quaternion.Euler(mainCamera.transform.eulerAngles) * Vector3.left;
+            transform.localPosition += new Vector3(direction.x, 0, direction.z) * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            var direction = Quaternion.Euler(mainCamera.transform.eulerAngles) * Vector3.right;
+            transform.localPosition += new Vector3(direction.x, 0, direction.z) * moveSpeed * Time.deltaTime;
+        }
     }
 }
