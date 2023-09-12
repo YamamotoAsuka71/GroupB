7c9d7f59d1b388819919436b0a771cba20b37642
nishiumi <xiyeliang737@gmail.com>
2023-09-12 10:03:48 +0900

    スクリプトにコメント追加しました

--- GroupB/Assets/Scripts/EnemyFactory.cs
+++ GroupB/Assets/Scripts/EnemyFactory.cs
@@ -1,16 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyFactory : MonoBehaviour
 {
     //生成するゲームオブジェクト
-    public GameObject red;
-    public GameObject blue;
-    public GameObject green;
-    public GameObject yellowllow;
-    public GameObject purple; 
-    public GameObject Variant;
+    public GameObject red;//赤色の敵
+    public GameObject blue;//青色の敵
+    public GameObject green;//緑色の敵
+    public GameObject yellowllow;//黄色の敵
+    public GameObject purple; //紫色の敵
+    public GameObject Variant;//空色の敵
     void Start()
     {
         //FunctionTestを3秒毎に呼び出します。
@@ -19,6 +17,7 @@ public class EnemyFactory : MonoBehaviour
 
     void Generation()
     {
+        //ランダムで数字を決める、そしてその数字で敵を決めて生成している
         float target = Random.Range(0, 975);
         if (target == 0||target<225)
         {
--- GroupB/Assets/Scripts/TimeCounter.cs
+++ GroupB/Assets/Scripts/TimeCounter.cs
@@ -11,18 +11,21 @@ public class TimeCounter : MonoBehaviour
     private void Start()
     {
         timeText = GetComponent<Text>();
-        countdownSeconds = countdownMinutes * 60;//‚±‚±‚Å‚P•ªi‚U‚O•bj‚ª‚R‰ñ
+        countdownSeconds = countdownMinutes * 60;//‚±‚±‚Å‚P•ªi‚U‚O•bj‚ª‚R‰ñ‚Å‚R•ª‚ğŒvZ‚µ‚Ä‚¢‚é
     }
 
     void Update()
     {
+        //‚±‚±‚Å2:30‚İ‚½‚¢‚È•\¦‚É‚µ‚Ä‚¢‚é
         countdownSeconds -= Time.deltaTime;
         var span = new TimeSpan(0, 0, (int)countdownSeconds);
         timeText.text = span.ToString(@"mm\:ss");
-        //GetComponent<TextMeshProUGUI>().text = "time:" + Timer.ToString("f2");
+
+@@@@//0•b‚É‚È‚Á‚½‚Æ‚«‚Ìˆ—
         if (countdownSeconds <= 0)
         {
-            // 0•b‚É‚È‚Á‚½‚Æ‚«‚Ìˆ—
+            //0•b‚É‚È‚Á‚½‚çÁ‚·
+            Destroy(timeText);
         }
     }
 }
